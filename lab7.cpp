767065d250e5f7c64980acb638ee17c19db3e072
Даниил Курьяков <double_di@MacBook-Pro-Daniil.local>
2022-09-10 13:15:22 +0300

    add cpp file

---
+++ lab7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define str_len 15
+#define size 30
+
+int choice;
+int order = 0;
+using namespace std;
+
+struct Zapbook
+{
+	char FIO[str_len];
+	char DateOfBirth[10];
+	char Address[size];
+	int NumberPhone;
+	char WorkStudy[30];
+	char Dolshnost[10];
+};
+ 
